639d349eae9e0f92b1f628e049f66f5b2e321458
Yoshito924 <kimu.murd.cisum@gmail.com>
2021-08-18 15:18:21 +0900

    修正

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 class Footer extends React.Component {
     render() {
