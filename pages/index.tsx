785e14e23faf9f72842f62ad15156624e9d2c2d6
Yoshito924 <kimu.murd.cisum@gmail.com>
2021-08-28 18:17:14 +0900

    ホームの余白の微調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,8 +38,8 @@ const Home: NextPage = () => {
 
         <Container fixed>
           <main className={styles.main}>
-            <div className="bg-white mt-2">
-              {/* <div className="lg:px-8 lg:pt-8">
+
+            {/* <div className="lg:px-8 lg:pt-8">
                 <Image
                   src="/yoshito.png"
                   alt="プロフィール写真"
@@ -48,80 +48,80 @@ const Home: NextPage = () => {
                   height={230}
                 />
               </div> */}
-              <div className="lg:flex">
-                <div className="sm:w-1/1 lg:w-8/12">
+            <div className="lg:flex">
+              <div className="sm:w-1/1 lg:w-8/12">
 
-                  <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Yoshito Kimura</h2>
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Yoshito Kimura</h2>
 
-                  <p className="mt-3 mb-6 text-gray-500">
-                    キムラ ヨシト
-                  </p>
+                <p className="mt-3 mb-6 text-gray-500">
+                  キムラ ヨシト
+                </p>
 
-                  <h3 className={styles.midashi}>主なプロフィール</h3>
+                <h3 className={styles.midashi}>主なプロフィール</h3>
 
-                  <dl className="mt-5 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-12 lg:gap-x-2">
-                    {features.map((feature: any) => (
-                      <div key={feature.name} className="border-t border-gray-300 pt-4">
-                        <dt className="font-medium text-gray-900">{feature.name}</dt>
-                        <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
-                      </div>
-                    ))}
-                  </dl>
+                <dl className="mt-5 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-12 lg:gap-x-2">
+                  {features.map((feature: any) => (
+                    <div key={feature.name} className="border-t border-gray-300 pt-4">
+                      <dt className="font-medium text-gray-900">{feature.name}</dt>
+                      <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+                    </div>
+                  ))}
+                </dl>
 
-                  <p className="my-2">　</p>
+                <p className="my-2">　</p>
 
-                  <h3 className={styles.midashi}>主な扱えるソフト</h3>
+                <h3 className={styles.midashi}>主な扱えるソフト</h3>
 
-                  <p className="mt-3 text-gray-500">
-                    <p>Word / Excel / PowerPoint</p>
-                    <p>Cubase / Finale / Cakewalk by BandLab</p>
-                    <p>Photoshop / Premiere Pro / Lightroom / Illustrator</p>
-                    <p>Xmind / drow.io </p>
-                    <p>Visual Studio Code / Google Colaboratory / Jupyter Lab</p>
-                  </p>
+                <p className="mt-3 text-gray-500">
+                  <p>Word / Excel / PowerPoint</p>
+                  <p>Cubase / Finale / Cakewalk by BandLab</p>
+                  <p>Photoshop / Premiere Pro / Lightroom / Illustrator</p>
+                  <p>Xmind / drow.io </p>
+                  <p>Visual Studio Code / Google Colaboratory / Jupyter Lab</p>
+                </p>
 
-                  <p className="my-2">　</p>
+                <p className="my-2">　</p>
 
-                  <h3 className={styles.midashi}>使用経験がある主な技術</h3>
+                <h3 className={styles.midashi}>使用経験がある主な技術</h3>
 
-                  <p className="mt-3 text-gray-500">
-                    <p>HTML / CSS / Bootstrap / TailwindCSS</p>
-                    <p>JavaScript / TypeScript / Vue.js / React / Next.js</p>
-                    <p>Python</p>
-                    <p>Git・GitHub</p>
-                  </p>
+                <p className="mt-3 text-gray-500">
+                  <p>HTML / CSS / Bootstrap / TailwindCSS</p>
+                  <p>JavaScript / TypeScript / Vue.js / React / Next.js</p>
+                  <p>Python</p>
+                  <p>Git・GitHub</p>
+                </p>
 
-                  <p className="my-2">　</p>
+                <p className="my-2">　</p>
 
-                  <h3 className={styles.midashi}>できることマインドマップ</h3>
-                  <p className="mt-3 text-gray-500">
-                    <p>「できる」と言っても、どの程度「できる」かは まちまちです。</p>
-                    <p>歴が10年以上、自分の中で比較的得意なものには★をつけています。</p>
-                  </p>
-                  <p className="my-2">　</p>
-                  <Image
-                    src="/できること.png"
-                    alt="できることをまとめたマインドマップ"
-                    className="bg-gray-100 rounded-lg"
-                    width={1496}
-                    height={871}
-                  />
+                <h3 className={styles.midashi}>できることマインドマップ</h3>
+                <p className="mt-3 text-gray-500">
+                  <p>「できる」と言っても、どの程度「できる」かは まちまちです。</p>
+                  <p>歴が10年以上、自分の中で比較的得意なものには★をつけています。</p>
+                </p>
+                <p className="my-2">　</p>
+                <Image
+                  src="/できること.png"
+                  alt="できることをまとめたマインドマップ"
+                  className="bg-gray-100 rounded-lg"
+                  width={1496}
+                  height={871}
+                />
 
-                  <p className="my-2">　</p>
-
-                </div>
-
-
-
-                <div className="sm:w-1/1 lg:w-1/12">　</div>
-
-                <div className="sm:w-1/1 lg:w-3/12">
-                  <RightColumn></RightColumn>
-                </div>
-
+                <p className="my-2">　</p>
 
               </div>
+
+
+
+              <div className="sm:w-1/1 lg:w-1/12">　</div>
+
+              <div className="sm:w-1/1 lg:w-3/12">
+                <RightColumn></RightColumn>
+              </div>
+
+
             </div>
+
           </main >
         </Container>
 
